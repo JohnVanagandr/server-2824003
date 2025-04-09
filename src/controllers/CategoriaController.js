d3f64b3cc3d3348db5790a545b4ec3239ea0b9f7
John Becerra <jfbeccera@gmail.com>
2025-04-08 21:07:18 -0500

    método crear y el middleware para validar los datos al crear, iniciamos el métod eliminar

--- src/controllers/CategoriaController.js
+++ src/controllers/CategoriaController.js
@@ -8,11 +8,18 @@ class CategoriaController {
     const categorias = await OBJCategoria.getAll();
     return res.json(categorias);
   }
-
+  // Método crear categoria
   static async createCategoria(req, res) {
     const { nombre, descripcion } = req.body;
     const OBJCategoria = new Categoria();
-    const categorias = await OBJCategoria.create(nombre, descripcion);
+    const categoria = await OBJCategoria.create(nombre, descripcion);
+    return res.json(categoria)
+  }
+  // Método eliminar categoria
+  static deleteCategoria(req, res) {
+    const { id } = req.params;
+    const OBJCategoria = new Categoria();
+    const categoria = OBJCategoria.delete(id);
   }
 
 }
--- src/models/Categoria.js
+++ src/models/Categoria.js
@@ -13,10 +13,35 @@ class Categoria {
     }
   }
   // Método -> crear una categoría
-  create(nombre, descripcion) {
-    connection.query("insert into categorias (nombre, descripcion) value (?, ?)", [nombre, descripcion]);
+  async create(nombre, descripcion) {
+    const [result] = await connection.query("insert into categorias (nombre, descripcion) value (?, ?)", [nombre, descripcion]);
+    return {
+      id: result.insertId,
+      nombre,
+      descripcion
+    }
+  }
 
+  async getById(id) {
+    try {
+      const [rows] = await connection.query("select * from categorias where id = ?", [id]);
+      if (rows.length === 0) {
+        throw new Error("Categoria no encontrada");
+      }
+      return rows[0];
+    } catch (error) {
+      throw new Error("Error al obtener la categoria");
+    }
+  }
 
+  estaRelacionadaConProductos(categoria_id) {
+    // select * from productos where categoria_id = 3
+  }
+
+  async delete(id) {
+    let datos = await this.getById(id)
+    // let tieneRelacion = this.estaRelacionadaConProductos()
+    console.log(datos);
   }
 }
 
